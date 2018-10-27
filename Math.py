f71952f92ca05f7e713e4a0ae4289e32e382417c
The20thDuck <37743253+The20thDuck@users.noreply.github.com>
2018-10-26 23:54:24 -0400

    Update Math.py

--- Math.py
+++ Math.py
@@ -18,6 +18,11 @@ def home():
 def algebra():
     return render_template('Algebra.html')
 
+@site.route('/vertex')
+def algebra():
+    return render_template('Vertex.html')
+
+
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	site.run(host="0.0.0.0", port=port, threaded=True)
