978f50d0cd919cbd96e3d317e031c5e68395d91a
The20thDuck <37743253+The20thDuck@users.noreply.github.com>
2018-10-26 22:57:24 -0400

    Update ChessAPI.py

--- ChessAPI.py
+++ ChessAPI.py
@@ -15,17 +15,6 @@ api_url = 'https://api.chess.com/pub/player/The20thDuck'
 def home():
     return render_template('Home.html')
 
-@site.route('/player', methods = ['POST', 'GET'])
-def player():
-    if request.method == 'POST':
-        playername = request.form
-        player_url = 'https://api.chess.com/pub/player/' + playername['Player']
-        player_data = requests.get(player_url).json()  
-        if 'player_id' in player_data.keys():
-            stat_data = requests.get(player_url + '/stats').json()
-            return render_template('Player.html', player = playername, datas = player_data, stats = stat_data)
-        else:
-            return render_template('Nonexistent.html')
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	site.run(host="0.0.0.0", port=port, threaded=True)
